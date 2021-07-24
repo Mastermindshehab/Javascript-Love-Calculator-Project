e63853268381312b05e91f16371074157be6d984
Mastermindshehab <84955917+Mastermindshehab@users.noreply.github.com>
2021-07-23 20:28:55 -0700

    Javascript Love Calculator Project
    added Html,Css,Javascript file for the Love Calculator Project

---
+++ love.js
@@ -0,0 +1,10 @@
+//const firstname=document.querySelector("#fname");
+//const Lastname=document.querySelector("#Lname");
+
+const firstinput=prompt("typer your name");
+const partnername=prompt("typer partner name");
+let number= Math.random()*100;
+
+number=Math.floor(number)+1;
+
+document.getElementById("#result").innerHTML=number;
